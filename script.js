778d7132d9972c69419635ecfa74abf075ef7045
Muhammad Khater <152961532+Muhammad-Khater@users.noreply.github.com>
2025-12-07 16:44:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* -------------------- Skills Animation on Scroll -------------------- */
 document.addEventListener("DOMContentLoaded", function () {
   const skillBars = document.querySelectorAll(".skill");
-  const circulars = document.querySelectorAll(".circle-skill");
   const skillsSection = document.getElementById("skills");
 
   function animateSkills() {
@@ -9,21 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const screenHeight = window.innerHeight;
 
     if (sectionPos < screenHeight - 50) {
-      // Linear Skills
       skillBars.forEach((skill) => {
         const percent = skill.getAttribute("data-percent");
         skill.querySelector(".skill-bar-new span").style.width = percent + "%";
       });
-
-      // Circular Skills
-      circulars.forEach((c) => {
-        const value = c.getAttribute("data-value");
-        const degree = value * 3.6;
-        c.style.background = `
-          radial-gradient(closest-side, #000 80%, transparent 0 99.9%, #000 0),
-          conic-gradient(#00eaff 0deg ${degree}deg, #222 ${degree}deg 360deg)
-        `;
-      }, 200);
 
       window.removeEventListener("scroll", animateSkills);
     }
